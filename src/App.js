60000d09be6337a237c3c3f0e4858e7f107581aa
Vanessa Stewart <vstewart1213@gmail.com>
2020-12-28 19:40:30 -0800

    Add README.md

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import BookList from './components/BookList';
 import Navbar from './components/Navbar';
+import ThemeContextProvider from './contexts/ThemeContext';
+import ThemeToggle from './components/ThemeToggle';
+
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <BookList />
+      <ThemeContextProvider>
+        <Navbar />
+        <BookList />
+        <ThemeToggle />
+      </ThemeContextProvider>
     </div>
   );
 }
---
+++ src/ThemeToggle.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { ThemeContext } from './contexts/ThemeContext';
+
+class ThemeToggle extends Component {
+  static contextType = ThemeContext;
+  render() { 
+    const { toggleTheme } = this.context;
+    return ( 
+      <button onClick={toggleTheme}>Toggle the theme</button>
+     );
+  }
+}
+ 
+export default ThemeToggle;
