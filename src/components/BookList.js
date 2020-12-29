1d3a8058eb341bbe604606b73bcbef1ccc3d4bfc
Vanessa Stewart <vstewart1213@gmail.com>
2020-12-29 12:36:44 -0800

    Refactor BookList component to use useContext hook

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,24 +1,39 @@
-import React, {Component} from 'react';
+import React, { useContext } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 
-class BookList extends Component {
-  static contextType = ThemeContext;
+const BookList = () => {
+  const { isLightTheme, light, dark } = useContext(ThemeContext)
+  const theme = isLightTheme ? light : dark;
 
-  render() { 
-    
-    const { isLightTheme, light, dark } = this.context;
-    const theme = isLightTheme ? light : dark;
-
-    return (  
+  return ( 
       <div className='book-list' style= {{color: theme.syntax, background: theme.bg}}>
-        <ul>
-          <li style={{background: theme.ui}}>the way of kings</li>
-          <li style={{background: theme.ui}}>the name of the wind</li>
-          <li style={{background: theme.ui}}>the final empire</li>
-        </ul>
-      </div>
-    );
-  }
+      <ul>
+        <li style={{background: theme.ui}}>the way of kings</li>
+        <li style={{background: theme.ui}}>the name of the wind</li>
+        <li style={{background: theme.ui}}>the final empire</li>
+      </ul>
+    </div>
+   );
 }
  
 export default BookList;
+
+// class BookList extends Component {
+//   static contextType = ThemeContext;
+
+//   render() { 
+    
+//     const { isLightTheme, light, dark } = this.context;
+//     const theme = isLightTheme ? light : dark;
+
+//     return (  
+//       <div className='book-list' style= {{color: theme.syntax, background: theme.bg}}>
+//         <ul>
+//           <li style={{background: theme.ui}}>the way of kings</li>
+//           <li style={{background: theme.ui}}>the name of the wind</li>
+//           <li style={{background: theme.ui}}>the final empire</li>
+//         </ul>
+//       </div>
+//     );
+//   }
+// }
