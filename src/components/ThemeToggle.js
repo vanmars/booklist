548a6ddf247d66c960ac878534651ddbeac7ef4b
Vanessa Stewart <vstewart1213@gmail.com>
2020-12-29 11:28:49 -0800

    Add authentication context

--- src/components/ThemeToggle.js
+++ src/components/ThemeToggle.js
@@ -2,11 +2,10 @@ import React, {Component} from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 
 class ThemeToggle extends Component {
-  static contexType = ThemeContext;
+  static contextType = ThemeContext;
   
   render() { 
     const { toggleTheme } = this.context;
-
     return ( 
       <button onClick={toggleTheme}>Toggle the theme</button>
      );
